80eca5977860859dadcab10c5c6635a73e65edb6
andreas-roennestad <a.roennestad@gmail.com>
2022-11-24 13:48:30 +0100

    .

--- load_dataset.py
+++ load_dataset.py
@@ -50,7 +50,6 @@ class RoadCracksDetection(torchvision.datasets.VisionDataset):
             tuple: (image, target) where target is a dictionary of the XML tree.
         """
         img = Image.open(self.images[index]).convert("RGB")
-        print(list(img.getdata()))
         target = self.parse_voc_xml(ET_parse(self.targets[index]).getroot())
 
         if self.transforms is not None:
--- main.py
+++ main.py
@@ -43,12 +43,14 @@ data_transforms = {
     'train': transforms.Compose([
         transforms.RandomResizedCrop(input_size),
         transforms.RandomHorizontalFlip(),
-        transforms.ToTensor()
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
     'val': transforms.Compose([
         transforms.Resize(input_size),
         transforms.CenterCrop(input_size),
-        transforms.ToTensor()
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
 }
 
