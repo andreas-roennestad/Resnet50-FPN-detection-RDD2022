d46184d476014685df6382313bbf2cad083db13a
andreas-roennestad <a.roennestad@gmail.com>
2022-11-24 10:52:44 +0100

    data

---
+++ finetune.py
@@ -0,0 +1,27 @@
+from __future__ import print_function
+from __future__ import division
+from load_dataset import RoadCracksDetection
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import torchvision
+from torchvision import models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+
+print("PyTorch Version: ",torch.__version__)
+print("Torchvision Version: ",torchvision.__version__)
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# Top level data directory. Here we assume the format of the directory conforms
+#   to the ImageFolder structure
+root_dir = "/cluster/projects/vc/courses/TDT17/2022/open/RDD2022/Norway/"
+
+
+dataset = RoadCracksDetection(root_dir)
+
+item = dataset.__getitem__(0)
+print(item[1])
---
+++ load_dataset.py
@@ -0,0 +1,87 @@
+import collections
+import os
+from xml.etree.ElementTree import Element as ET_Element
+
+from torchvision.datasets import VisionDataset
+try:
+    from defusedxml.ElementTree import parse as ET_parse
+except ImportError:
+    from xml.etree.ElementTree import parse as ET_parse
+from typing import Any, Callable, Dict, List, Optional, Tuple
+
+from PIL import Image
+
+
+
+
+class RoadCracksDetection(VisionDataset):
+
+    def __init__(
+        self,
+        root: str,
+        image_set: str = "train",
+        transform: Optional[Callable] = None,
+        target_transform: Optional[Callable] = None,
+        transforms: Optional[Callable] = None,
+    ):
+        super().__init__(root, transforms, transform, target_transform) 
+
+
+        splits_dir = os.path.join(root, image_set,"images")
+        split_f = os.path.join(splits_dir, image_set.rstrip("\n") + ".txt")
+
+
+        with open(os.path.join(split_f)) as f:
+            file_names = [x.strip() for x in f.readlines()]
+
+        print(file_names)
+
+        image_dir = os.path.join(root, image_set, "images")
+        self.images = [os.path.join(image_dir, x + ".jpg") for x in file_names]
+
+        if image_set=="train":
+            target_dir = os.path.join(root, image_set, "annotations", "xmls")
+            self.targets = [os.path.join(target_dir, x + ".xml") for x in file_names]
+
+        assert len(self.images) == len(self.targets)
+
+
+
+    def __getitem__(self, index: int) -> Tuple[Any, Any]:
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target) where target is a dictionary of the XML tree.
+        """
+        img = Image.open(self.images[index]).convert("RGB")
+        target = self.parse_voc_xml(ET_parse(self.annotations[index]).getroot())
+
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+
+        return img, target   
+
+    def __len__(self) -> int:
+        return len(self.images)   
+
+
+    @staticmethod
+    def parse_voc_xml(node: ET_Element) -> Dict[str, Any]:
+        xml_dict: Dict[str, Any] = {}
+        children = list(node)
+        if children:
+            def_dic: Dict[str, Any] = collections.defaultdict(list)
+            for dc in map(RoadCracksDetection.parse_voc_xml, children):
+                for ind, v in dc.items():
+                    def_dic[ind].append(v)
+            if node.tag == "annotation":
+                def_dic["object"] = [def_dic["object"]]
+            xml_dict = {node.tag: {ind: v[0] if len(v) == 1 else v for ind, v in def_dic.items()}}
+        if node.text:
+            text = node.text.strip()
+            if not children:
+                xml_dict[node.tag] = text
+        return xml_dict  
+
