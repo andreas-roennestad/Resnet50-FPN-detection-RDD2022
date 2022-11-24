cdfbc76fd77fb24682140509f915a6ee79ab4dd3
andreas-roennestad <a.roennestad@gmail.com>
2022-11-24 12:08:27 +0100

    s

--- finetune.py
+++ finetune.py
@@ -24,5 +24,6 @@ root_dir = "/cluster/projects/vc/courses/TDT17/2022/open/RDD2022/Norway/"
 dataset = RoadCracksDetection(root_dir)
 
 item = dataset.__getitem__(0)
-print(item[0])
-print(item[1])
+
+for i in range(4):
+    print(dataset[i])
