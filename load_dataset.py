669b28b9fdafe2c81cd358c1e330e126fc432191
andreas-roennestad <a.roennestad@gmail.com>
2022-11-24 15:57:30 +0100

    .

--- load_dataset.py
+++ load_dataset.py
@@ -68,7 +68,7 @@ class RoadCracksDetection(torchvision.datasets.VisionDataset):
 
     def parse_dict(self, xml_out_dict: dict) -> dict[str, Any]:
         in_dict = xml_out_dict['annotation']
-        out_dict = {'labels': np.array([]), 'boxes': np.array([]), 'image_id': np.array([]), 'area': np.array([]), 'iscrowd': np.array([])}
+        out_dict = {'labels': np.empty(), 'boxes': np.empty(), 'image_id': np.empty(), 'area': np.empty(), 'iscrowd': np.empty()}
         for obj in in_dict['object']:
             match obj['name']:
                 case 'D00':
