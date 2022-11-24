f6a16ab0f867bd3ee6f019bed6d3fc86e9cf2711
andreas-roennestad <a.roennestad@gmail.com>
2022-11-24 16:41:34 +0100

    .

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ data_transforms = {
     'train': transforms.Compose([
         #transforms.RandomResizedCrop(input_size),
         #transforms.RandomHorizontalFlip(),
-        #transforms.Resize(input_size),
+        transforms.Resize(input_size),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
