fde61c07302a710a560070fb7dcb8cbfce9ba4de
andreas-roennestad <a.roennestad@gmail.com>
2022-11-25 10:01:26 +0100

    .

--- load_dataset.py
+++ load_dataset.py
@@ -87,7 +87,7 @@ class RoadCracksDetection(torchvision.datasets.VisionDataset):
             print(images)
 
 
-        return images, targets
+        return torch.tensor(images), torch.tensor(targets)
 
     def parse_dict(self, xml_out_dict: dict) -> dict[str, Any]:
         in_dict = xml_out_dict['annotation']
