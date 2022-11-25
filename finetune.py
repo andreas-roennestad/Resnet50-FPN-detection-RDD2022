2eefbab37e578d9f4c8ab819e7fcc1ff37eaae7e
andreas-roennestad <a.roennestad@gmail.com>
2022-11-25 10:22:16 +0100

    .

--- finetune.py
+++ finetune.py
@@ -62,7 +62,7 @@ def train_model(model, dataloader, criterion, optimizer, num_epochs=25):
             # Iterate over data.
             for inputs, labels in dataloader:
                 inputs = inputs.to(device)
-                labels = labels.move_to(device)
+                labels = move_to(labels, device)
 
                 # zero the parameter gradients
                 optimizer.zero_grad()
