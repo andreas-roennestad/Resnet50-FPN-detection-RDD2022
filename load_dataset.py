5f71d72ecafee93b5f23d3deefaa476feaef5789
andreas-roennestad <a.roennestad@gmail.com>
2022-11-25 09:39:44 +0100

    .

--- load_dataset.py
+++ load_dataset.py
@@ -75,7 +75,7 @@ class RoadCracksDetection(torchvision.datasets.VisionDataset):
         return len(self.images) 
 
     def collate_fn(self, batch): 
-        return tuple(zip(*batch))
+        return zip(*batch)
 
         #return images, targets
 
@@ -97,7 +97,7 @@ class RoadCracksDetection(torchvision.datasets.VisionDataset):
             out_dict['area'].append((int(float(obj['bndbox']['xmax']))-int(float(obj['bndbox']['xmin'])))*(int(float(obj['bndbox']['ymax']))- int(float(obj['bndbox']['ymin']))))
             out_dict['iscrowd'].append(False)
         out_dict['image_id'] = int(in_dict['filename'].replace('.jpg', '')[-6:])
-        print(out_dict['image_id'], in_dict['filename'])
+        #print(out_dict['image_id'], in_dict['filename'])
 
         return out_dict
     @staticmethod
