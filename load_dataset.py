110502291ca67b1d2b0ff3cc51abe489e6c2b087
andreas-roennestad <a.roennestad@gmail.com>
2022-11-25 09:49:19 +0100

    .

--- load_dataset.py
+++ load_dataset.py
@@ -82,7 +82,7 @@ class RoadCracksDetection(torchvision.datasets.VisionDataset):
             targets.append(b[1])
         
         try:
-            images = pad_sequence(images, batch_first=True)
+            images = torch.from_numpy(np.asarray(targets))(pad_sequence(images, batch_first=True))
         except RuntimeError:
             print(images)
 
