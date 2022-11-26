295fff36c4882af6c37982ef646588548dcc3d0f
andreas-roennestad <a.roennestad@gmail.com>
2022-11-26 14:56:37 +0100

    data

--- finetune.py
+++ finetune.py
@@ -1,8 +1,6 @@
 from __future__ import print_function
 from __future__ import division
 
-from werkzeug import test
-
 from load_dataset import RoadCracksDetection
 import torch
 import torch.nn as nn
