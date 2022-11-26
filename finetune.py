1518c826079ce8b37ca55568d7bdda89f900d38d
andreas-roennestad <a.roennestad@gmail.com>
2022-11-26 11:54:46 +0100

    data

--- finetune.py
+++ finetune.py
@@ -150,7 +150,7 @@ def train_step(model: torch.nn.Module,
     train_loss, train_acc = 0, 0
 
     # Loop through data loader data batches
-    for batch, (X, y) in enumerate(dataloader):
+    for batch, (X, y) in tqdm(enumerate(dataloader)):
         # Send data to target device
         X = X.to(device)
         y = move_to(y, device)
