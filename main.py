7a3d443f266f146764c83af8c7a9b3f56c1c4b9b
andreas-roennestad <a.roennestad@gmail.com>
2022-11-26 16:24:19 +0100

    data

--- main.py
+++ main.py
@@ -77,15 +77,15 @@ data_transforms = FasterRCNN_ResNet50_FPN_Weights.DEFAULT.transforms()
 
 dataset = RoadCracksDetection(root_dir, "train", transforms=data_transforms)
 #dataset_test = RoadCracksDetection(root_dir, "test", transforms=data_transforms)
-s_dataset = Subset(dataset, indices=range(len(dataset) // 20))
-s_dataset_test = Subset(dataset, indices=range(len(dataset) // 20, len(dataset) // 20 + 100))
+s_dataset = Subset(dataset, indices=range(len(dataset) // 30))
+s_dataset_test = Subset(dataset, indices=range(len(dataset) // 30, len(dataset) // 30 + 100))
 print("Length training data: ", len(s_dataset))
 print("Length test data: ", len(s_dataset_test))
 
 
 # Create training and validation dataloaders
-dataloader = torch.utils.data.DataLoader(s_dataset, batch_size=batch_size, shuffle=False, num_workers=4, collate_fn=dataset.collate_fn)
-dataloader_test = torch.utils.data.DataLoader(s_dataset_test, batch_size=batch_size, shuffle=False, num_workers=4, collate_fn=dataset.collate_fn)
+dataloader = torch.utils.data.DataLoader(s_dataset, batch_size=batch_size, shuffle=True, num_workers=4, collate_fn=dataset.collate_fn)
+dataloader_test = torch.utils.data.DataLoader(s_dataset_test, batch_size=batch_size, shuffle=True, num_workers=4, collate_fn=dataset.collate_fn)
 
 print("Len dataloader training: ", len(dataloader))
 print("Len dataloader test: ", len(dataloader_test))
