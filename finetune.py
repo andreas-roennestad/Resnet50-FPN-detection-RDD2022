28bdaf94441373ff09ad58df88c90406b8cb26a2
andreas-roennestad <a.roennestad@gmail.com>
2022-11-26 16:53:29 +0100

    data

--- finetune.py
+++ finetune.py
@@ -214,6 +214,7 @@ def test_step(model: torch.nn.Module,
             X = move_to(X, device)
             y = move_to(y, device)
     
+    
             # 1. Forward pass
             print("Predictions:\n")
             predictions = model(X)
