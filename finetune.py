e69f1f8fa7dd51d245907ff6e5ff64cccbbaa322
andreas-roennestad <a.roennestad@gmail.com>
2022-11-26 11:20:28 +0100

    data

--- finetune.py
+++ finetune.py
@@ -155,13 +155,13 @@ def train_step(model: torch.nn.Module,
         X = X.to(device)
         y = move_to(y, device)
         # 1. Forward pass
+        print(y)
         y_pred = model(X, y)
         print(y_pred)
         # 2. Calculate  and accumulate loss
 
         loss = y_pred['loss_classifier']#loss_fn(y_pred, y)
         train_loss += loss.item()
-        print(train_loss)
         # 3. Optimizer zero grad
         optimizer.zero_grad()
 
