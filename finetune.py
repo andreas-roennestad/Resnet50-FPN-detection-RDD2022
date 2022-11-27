65785f97cf8403bae189218ee207ff6ea9eb9d5a
andreas-roennestad <a.roennestad@gmail.com>
2022-11-27 15:28:52 +0100

    data

--- finetune.py
+++ finetune.py
@@ -219,6 +219,7 @@ def test_step(model: torch.nn.Module,
             
                 # Send data to target device
                 #print(X[0])
+                print(X)
                 X = move_to(X, device)
                 #y = move_to(y, device)
         
