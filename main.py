f65e6e0a9bce029c8c16a03281aba46bdb3990aa
andreas-roennestad <a.roennestad@gmail.com>
2022-11-27 23:41:34 +0100

    data

--- main.py
+++ main.py
@@ -93,7 +93,7 @@ else:
             print("\t",name)
 
 # Observe that all parameters are being optimized
-optimizer_ft = optim.SGD(params_to_update, lr=0.01)
+optimizer_ft = optim.SGD(params_to_update, lr=0.00001)
 
 loss_fn = nn.CrossEntropyLoss()
 
