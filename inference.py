a50774c48f8c05da7af710194dcd003ff9b325af
andreas-roennestad <a.roennestad@gmail.com>
2022-11-27 19:47:37 +0100

    data

--- inference.py
+++ inference.py
@@ -54,7 +54,7 @@ print("Length test data: ", len(dataset_test))
 
 
 # Create validation dataloaders
-dataloader_test = torch.utils.data.DataLoader(dataset_test, batch_size=8, shuffle=False, num_workers=4, collate_fn=dataset_test.collate_fn)
+dataloader_test = torch.utils.data.DataLoader(dataset_test, batch_size=1, shuffle=False, num_workers=0, collate_fn=dataset_test.collate_fn)
 print("Len dataloader test: ", len(dataloader_test))
 
 
