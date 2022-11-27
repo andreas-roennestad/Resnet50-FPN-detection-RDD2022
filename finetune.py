6dd9da009e2dad0e529022e77be20dfd4df8ee91
andreas-roennestad <a.roennestad@gmail.com>
2022-11-27 13:30:11 +0100

    data

--- finetune.py
+++ finetune.py
@@ -208,20 +208,23 @@ def test_step(model: torch.nn.Module,
     test_loss, test_acc = 0, 0
 
     # Turn on inference context manager
-    with torch.inference_mode():
-        # Loop through DataLoader batches
-        for batch, (X, y) in tqdm(enumerate(dataloader)):
-        
-            # Send data to target device
-            #print(X[0])
-            X = move_to(X, device)
-            y = move_to(y, device)
-    
+    with torch.no_grad():
+        with torch.inference_mode():
+            model= model.cuda()
+            # Loop through DataLoader batches
+            for batch, (X, y) in tqdm(enumerate(dataloader)):
 
-            # 1. Forward pass
-            predictions = model(X)
-            print(predictions)
-            # transport to cpu and save xmls
+            
+                # Send data to target device
+                #print(X[0])
+                X = move_to(X, device)
+                y = move_to(y, device)
+        
+
+                # 1. Forward pass
+                predictions = model(X)
+                print(predictions)
+                # transport to cpu and save xmls
             
 
 
