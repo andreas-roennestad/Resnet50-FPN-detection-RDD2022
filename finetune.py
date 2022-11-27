5eb9184ced7a5e6d3e22ff4ad33695d984d60f06
andreas-roennestad <a.roennestad@gmail.com>
2022-11-27 16:27:51 +0100

    data

--- finetune.py
+++ finetune.py
@@ -236,7 +236,7 @@ def test_step(model: torch.nn.Module,
                         if scores[s] > 0.1:     
                             b = boxes[s].cpu().numpy()
                             l = labels[s].cpu().numpy()
-                            line = [str(l[s]) + " " + str(b[0]) + " " + str(b[1]) + " " + str(b[2]) + " " + str(b[3])]
+                            line = str(l[s]) + " " + str(b[0]) + " " + str(b[1]) + " " + str(b[2]) + " " + str(b[3])
                             with open(predictions_file, 'w', newline='') as file:
                                 writer = csv.writer(file)
                                 writer.writerow([f_name, line])
