551e39cbea3c18e7f897c313b6ce0a7d278c8d03
andreas-roennestad <a.roennestad@gmail.com>
2022-11-27 17:20:35 +0100

    data

--- finetune.py
+++ finetune.py
@@ -235,7 +235,7 @@ def test_step(model: torch.nn.Module,
                     line = ""
                     
                     for s in range(len(scores)):
-                        if scores[s] > 0:     
+                        if scores[s] > 0.05:     
                             b = boxes[s].cpu().numpy()
                             l = labels[s].cpu().numpy()
                             line += str(l) + ' '
