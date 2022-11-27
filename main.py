54e95dcc37907369722afefdeb285fcb437c8564
andreas-roennestad <a.roennestad@gmail.com>
2022-11-27 19:59:40 +0100

    data

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ num_classes = 5
 # Batch size for training (change depending on how much memory you have)
 batch_size = 1
 # Number of epochs to train for
-num_epochs = 3
+num_epochs = 16
 
 # Flag for feature extracting. When False, we finetune the whole model,
 #   when True we only update the reshaped layer params
@@ -53,14 +53,14 @@ data_transforms = FasterRCNN_ResNet50_FPN_Weights.DEFAULT.transforms()
 
 
 dataset = RoadCracksDetection(root_dir, 'train', transforms=data_transforms)
-s_dataset = Subset(dataset, indices=range(0, len(dataset)))
+s_dataset = Subset(dataset, indices=range(0, len(dataset)//10))
 #s_dataset_test = Subset(dataset, indices=range(len(dataset)//5, len(dataset)//5))
 print("Length training data: ", len(s_dataset))
 #print("Length test data: ", len(s_dataset_test))
 
 
 # Create training and validation dataloaders
-dataloader = torch.utils.data.DataLoader(s_dataset, batch_size=batch_size, shuffle=False, num_workers=8, collate_fn=dataset.collate_fn)
+dataloader = torch.utils.data.DataLoader(s_dataset, batch_size=batch_size, shuffle=False, num_workers=16, collate_fn=dataset.collate_fn)
 #dataloader_test = torch.utils.data.DataLoader(s_dataset_test, batch_size=batch_size, shuffle=True, num_workers=4, collate_fn=dataset.collate_fn)
 
 print("Len dataloader training: ", len(dataloader))
