19ca1b5481e5bf8580d12b584fc088bf81898761
andreas-roennestad <a.roennestad@gmail.com>
2022-11-27 14:45:26 +0100

    data

--- inference.py
+++ inference.py
@@ -39,7 +39,7 @@ else:
     with open(save_file,"rb") as file:
         model_ft =models.detection.fasterrcnn_resnet50_fpn(weights=FasterRCNN_ResNet50_FPN_Weights.DEFAULT)
         num_ftrs = model_ft.roi_heads.box_predictor.bbox_pred.in_features
-        model_ft.roi_heads.box_predictor = FastRCNNPredictor(num_ftrs, num_classes)
+        #model_ft.roi_heads.box_predictor = FastRCNNPredictor(num_ftrs, num_classes)
         model_ft = model_ft.load_state_dict(torch.load(file))
         print(model_ft)
 
