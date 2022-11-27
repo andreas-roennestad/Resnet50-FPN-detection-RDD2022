d4e9c3f5f08dde73af44ad17a14b09980ecc0063
andreas-roennestad <a.roennestad@gmail.com>
2022-11-27 15:31:10 +0100

    data

--- finetune.py
+++ finetune.py
@@ -214,7 +214,7 @@ def test_step(model: torch.nn.Module,
     with torch.no_grad():
         with torch.inference_mode():
             # Loop through DataLoader batches
-            for batch, (X, f_name) in tqdm(enumerate(dataloader)):
+            for batch, (X) in tqdm(enumerate(dataloader)):
 
             
                 # Send data to target device
