f6ea0cb00bdf871508e493f3e09cc9ee50eaf67f
andreas-roennestad <a.roennestad@gmail.com>
2022-11-27 20:19:12 +0100

    data

--- inference.py
+++ inference.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torchvision
 from torchvision import models, transforms
-from finetune import set_parameter_requires_grad, train_model, train,test
+from finetune import set_parameter_requires_grad, train,test
 from torchvision.models.detection import fasterrcnn_resnet50_fpn, FasterRCNN_ResNet50_FPN_Weights
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 from load_dataset import RoadCracksDetection
