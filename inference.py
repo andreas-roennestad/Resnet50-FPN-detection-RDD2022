be20d3a98aea99106db80a521f7e6c64ce54aea3
andreas-roennestad <a.roennestad@gmail.com>
2022-11-29 20:08:20 +0100

    .

--- inference.py
+++ inference.py
@@ -24,7 +24,7 @@ save_file = "/cluster/work/andronn/VisualIntelligence/resnet_fpn_model_1.pkl"
 num_classes = 5
 
 # Batch size for training (change depending on how much memory you have)
-batch_size = 6
+batch_size = 1
 
 # Number of epochs to train for
 num_epochs = 1
